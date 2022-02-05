1507c44f8bab3a28f7028c4665edf6451eb9ffc2
drawingtoptarp <seanpburns@icloud.com>
2022-02-05 15:02:04 -0500

    Began work on game items
    Created sprite factory for items and imported item sprite sheet

---
+++ LegendOfZelda/Content/Items/ItemSpriteFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda.Content.Items
+{
+    public class ItemSpriteFactory
+    {
+        private Texture2D itemSpriteSheet;
+
+        private static ItemSpriteFactory instance = new ItemSpriteFactory();
+
+        public static ItemSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private ItemSpriteFactory()
+        {
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            itemSpriteSheet = content.Load<Texture2D>("itemSpriteSheet");
+        }
+    }
+}
