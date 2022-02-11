3edb42a0f6db11e11e72399941d71c89f2ffc55c
pupu98 <77910363+pupu98@users.noreply.github.com>
2022-02-11 15:10:20 -0500

    Add BasicLinkSprite

---
+++ LegendOfZelda/Content/Links/Sprite/BasicLinkSprite.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda.Content.Links.Sprite
+{
+    public abstract class BasicLinkSprite: ISprite
+    {
+        protected virtual Vector2 Pos { get; set; }
+        public virtual Vector2 Position
+        {
+            get { return new Vector2(Pos.X, Pos.Y); }
+            set { Pos = value; }
+        }
+        public virtual Texture2D Texture { get; set; }
+
+        protected virtual int Rows { get; set; }
+        protected virtual int Columns { get; set; }
+        protected virtual int CurrentFrame { get; set; }
+        protected virtual int TotalFrames { get; set; }
+
+        
+        public virtual void Update()
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = CurrentFrame / Columns;
+            int column = CurrentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)Pos.X, (int)Pos.Y, width, height);
+
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+
+        }
+    }
+}
+
--- LegendOfZelda/Content/Links/Sprite/RightWalkLinkSprite.cs
+++ LegendOfZelda/Content/Links/Sprite/RightWalkLinkSprite.cs
@@ -6,49 +6,21 @@ using System.Text;
 
 namespace LegendOfZelda.Content.Links.Sprite
 {
-    class RightWalkLinkSprite : ISprite
+    class RightWalkLinkSprite : BasicLinkSprite
     {
-        private Vector2 Pos;
-        public Vector2 Position
-        {
-            get { return Pos; }
-            set { Pos = value; }
-        }
-
-        public Texture2D Texture { get; set; }
-
-        public int Rows { get; set; }
-        public int Columns { get; set; }
-        private int currentFrame;
-        private int totalFrames;
-
         public RightWalkLinkSprite(Texture2D texture, Vector2 Position)
         {
-            Texture = texture;
             Rows = 1;
             Columns = 2;
-            currentFrame = 0;
-            totalFrames = Rows * Columns;
-
-            this.Position = Position;
+            CurrentFrame = 0;
+            TotalFrames = Rows * Columns;
+            Texture = texture;
+            Pos = Position;
         }
-        public void Update()
+        public override void Update()
         {
-            Pos.X = Pos.X + 3;
-            currentFrame = (currentFrame + 1) % totalFrames;
-        }
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            int width = Texture.Width / Columns;
-            int height = Texture.Height / Rows;
-            int row = currentFrame / Columns;
-            int column = currentFrame % Columns;
-
-            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
-            Rectangle destinationRectangle = new Rectangle((int)Pos.X, (int)Pos.Y, width, height);
-
-            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
-
+            Pos = new Vector2(Pos.X + 3, Pos.Y);
+            CurrentFrame = (CurrentFrame + 1) % TotalFrames;
         }
     }
 }
