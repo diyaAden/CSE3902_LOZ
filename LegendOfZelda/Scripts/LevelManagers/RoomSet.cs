7ff50d8a3eb3d2d865c8117547ac6a97e7d17024
drawingtoptarp <seanpburns@icloud.com>
2022-02-24 10:44:21 -0500

    Began work with xml room design

---
+++ LegendOfZelda/Scripts/LevelManagers/RoomSet.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+
+namespace LegendOfZelda.Scripts.LevelManagers
+{
+    class RoomSet
+    {
+        private readonly string winDir = System.Environment.GetEnvironmentVariable("windir");
+        private XmlReader room1;
+
+        public RoomSet()
+        {
+        }
+        public void LoadContent()
+        {
+            room1 = XmlReader.Create(winDir + "\\Room1.xml");
+        }
+    }
+}
