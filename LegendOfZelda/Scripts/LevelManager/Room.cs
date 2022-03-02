baa4673e01fc89efbd4824da3903afb77bdcbb8f
drawingtoptarp <seanpburns@icloud.com>
2022-03-02 10:58:59 -0500

    XML files can now be read correctly
    As long as the naming conventions are kept consistant

--- LegendOfZelda/Scripts/LevelManager/Room.cs
+++ LegendOfZelda/Scripts/LevelManager/Room.cs
@@ -1,6 +1,7 @@
 ﻿using LegendOfZelda.Scripts.Blocks;
 using LegendOfZelda.Scripts.Enemy;
 using LegendOfZelda.Scripts.Items;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 
@@ -19,36 +20,40 @@ namespace LegendOfZelda.Scripts.LevelManager
             Blocks = new List<IBlock>();
         }
 
+        public void AddObject(string type, string name, int xPos, int yPos)
+        {
+            if (type == "Item") AddItem(name, xPos, yPos);
+            else if (type == "Block") AddBlock(name, xPos, yPos);
+            else if (type == "Enemy") AddEnemy(name, xPos, yPos);
+        }
+        private void AddItem(string name, int xPos, int yPos)
+        {
+            Items.Add(ItemSpriteFactory.Instance.CreateItemFromString(name));
+            Items[^1].position = new Vector2(xPos, yPos);
+        }
+        private void AddBlock(string name, int xPos, int yPos)
+        {
+            Blocks.Add(BlockSpriteFactory.Instance.CreateBlockFromString(name));
+            Blocks[^1].position = new Vector2(xPos, yPos);
+        }
+        private void AddEnemy(string name, int xPos, int yPos)
+        {
+            //Enemies.Add(EnemySpriteFactory.Instance.CreateEnemyFromString(name));
+            //Enemies[^1].position = new Vector2(xPos, yPos);
+        }
+
         public void Update()
         {
-            foreach (IItem item in Items)
-            {
-                item.Update();
-            }
-            foreach (IBlock block in Blocks)
-            {
-                block.Update();
-            }
-            foreach (IEnemy enemy in Enemies)
-            {
-                enemy.Update();
-            }
+            foreach (IItem item in Items) item.Update();
+            foreach (IBlock block in Blocks) block.Update();
+            foreach (IEnemy enemy in Enemies) enemy.Update();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            foreach (IItem item in Items)
-            {
-                item.Draw(spriteBatch);
-            }
-            foreach (IBlock block in Blocks)
-            {
-                block.Draw(spriteBatch);
-            }
-            foreach (IEnemy enemy in Enemies)
-            {
-                enemy.Draw(spriteBatch);
-            }
+            foreach (IItem item in Items) item.Draw(spriteBatch);
+            foreach (IBlock block in Blocks) block.Draw(spriteBatch);
+            foreach (IEnemy enemy in Enemies) enemy.Draw(spriteBatch);
         }
     }
 }
--- LegendOfZelda/Scripts/LevelManager/RoomManager.cs
+++ LegendOfZelda/Scripts/LevelManager/RoomManager.cs
@@ -1,5 +1,4 @@
-﻿using LegendOfZelda.Scripts.LevelManager.RoomDataClasses;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Xml;
@@ -8,40 +7,38 @@ namespace LegendOfZelda.Scripts.LevelManager
 {
     public class RoomManager
     {
-        private readonly RoomCreator roomCreator;
         private XmlReader xml;
         public List<Room> Rooms { get; private set; }
         public int CurrentRoom { get; set; }
         public RoomManager() 
         {
             Rooms = new List<Room>();
-            roomCreator = new RoomCreator();
-            CurrentRoom = 0;
+            CurrentRoom = 1;
         }
         public void LoadContent(ContentManager content)
         {
             /* Room 0 is the dev room */
-            for (int i = 0; i <= 0; i++) {
-                xml = XmlReader.Create("Content\\XML\\Room0.xml");
-
-                xml.MoveToContent();
-                while (xml.Name != "ObjectType") xml.Read();
-                var data1 = xml.ReadElementContentAsString();
-                while (xml.Name != "ObjectName") xml.Read();
-                var data2 = xml.ReadElementContentAsString();
-                while (xml.Name != "PositionX") xml.Read();
-                var data3 = xml.ReadElementContentAsInt();
-                while (xml.Name != "PositionY") xml.Read();
-                var data4 = xml.ReadElementContentAsInt();
-
-
-                System.Console.WriteLine(data1); 
-                System.Console.WriteLine(data2); 
-                System.Console.WriteLine(data3); 
-                System.Console.WriteLine(data4);
-
+            for (int i = 0; i <= 1; i++) {
+                xml = XmlReader.Create("Content\\XML\\Room" + i + ".xml");
+                string objectType = "", objectName = "";
+                int posX = -1, posY = -1;
                 Room room = new Room();
-                //roomCreator.InsertRoomObjects(currentRoomData, room);
+                xml.MoveToContent();
+                xml.Read();
+                while (xml.IsStartElement()) {
+                    while (xml.Name != "ObjectType") xml.Read();
+                    objectType = xml.ReadElementContentAsString();
+                    while (xml.Name != "ObjectName") xml.Read();
+                    objectName = xml.ReadElementContentAsString();
+                    while (xml.Name != "PositionX") xml.Read();
+                    posX = xml.ReadElementContentAsInt();
+                    while (xml.Name != "PositionY") xml.Read();
+                    posY = xml.ReadElementContentAsInt();
+                    while(xml.Name != "Item") xml.Read();
+                    xml.Read();
+                    xml.Read();
+                    room.AddObject(objectType, objectName, posX, posY);
+                }
                 Rooms.Add(room);
             }
         }
