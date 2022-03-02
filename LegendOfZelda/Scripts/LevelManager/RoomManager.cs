c0fbe00c28bdd44dbd01bd152dc1f8ca4795537b
drawingtoptarp <seanpburns@icloud.com>
2022-03-02 09:31:39 -0500

    xml work

--- LegendOfZelda/Scripts/LevelManager/RoomManager.cs
+++ LegendOfZelda/Scripts/LevelManager/RoomManager.cs
@@ -2,12 +2,14 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace LegendOfZelda.Scripts.LevelManager
 {
     public class RoomManager
     {
         private readonly RoomCreator roomCreator;
+        private XmlReader xml;
         public List<Room> Rooms { get; private set; }
         public int CurrentRoom { get; set; }
         public RoomManager() 
@@ -19,10 +21,16 @@ namespace LegendOfZelda.Scripts.LevelManager
         public void LoadContent(ContentManager content)
         {
             /* Room 0 is the dev room */
-            for (int i = 0; i <= 1; i++) {
-                RoomData currentRoomData = content.Load<RoomData>("XML/Room" + i);
+            for (int i = 0; i <= 0; i++) {
+                xml = XmlReader.Create("Content\\XML\\Room0.xml");
+
+                xml.MoveToContent();
+                string data = xml.ReadElementContentAsString();
+
+                System.Console.WriteLine(data);
+
                 Room room = new Room();
-                roomCreator.InsertRoomObjects(currentRoomData, room);
+                //roomCreator.InsertRoomObjects(currentRoomData, room);
                 Rooms.Add(room);
             }
         }
