267024e722279d219809956f7ecca3b729e97777
drawingtoptarp <seanpburns@icloud.com>
2022-03-02 10:01:23 -0500

    xml work

--- LegendOfZelda/Scripts/Blocks/BlockSpriteFactory.cs
+++ LegendOfZelda/Scripts/Blocks/BlockSpriteFactory.cs
@@ -7,7 +7,7 @@ namespace LegendOfZelda.Scripts.Blocks
     class BlockSpriteFactory
     {
         private Texture2D blockSpriteSheet, fireSpriteSheet;
-        private static readonly BlockSpriteFactory instance = new BlockSpriteFactory();
+        private static readonly BlockSpriteFactory instance = new BlockSpriteFactory(); 
         public static BlockSpriteFactory Instance => instance;
 
         private BlockSpriteFactory()
