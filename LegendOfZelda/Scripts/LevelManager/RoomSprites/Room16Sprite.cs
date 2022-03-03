9152d1d4bdbc1a9f971dfe009acc1c958cdc13a8
benroth012 <benroth012@gmail.com>
2022-03-03 17:19:30 -0500

    Rooms 16-17
    Finished rooms 16 and 17

---
+++ LegendOfZelda/Scripts/LevelManager/RoomSprites/Room16Sprite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LegendOfZelda.Scripts.LevelManager
+{
+    public class Room16Sprite : BasicRoomBackground
+    {
+        public Room16Sprite(Texture2D DungeonMap)
+        {
+            spriteSheet = DungeonMap;
+            sourceRect = new Rectangle(258, 0, 255, 176);
+        }
+        public override void Update() { }
+    }
+}
