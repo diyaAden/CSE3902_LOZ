b833a7046f9490249eb0c97b725996a9f2d9f6fa
drawingtoptarp <seanpburns@icloud.com>
2022-03-05 20:52:43 -0500

    Fixed room 9

--- LegendOfZelda/Scripts/LevelManager/RoomManager.cs
+++ LegendOfZelda/Scripts/LevelManager/RoomManager.cs
@@ -11,7 +11,7 @@ namespace LegendOfZelda.Scripts.LevelManager
         public int CurrentRoom { get; set; }
         public RoomManager()
         {
-            CurrentRoom = 8;
+            CurrentRoom = 9;
         }
         public void LoadContent()
         {
