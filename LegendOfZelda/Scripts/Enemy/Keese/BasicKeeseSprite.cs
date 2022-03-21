61fa24aff201e5c90f7cd6bf7c132d856a7cbe9b
drawingtoptarp <seanpburns@icloud.com>
2022-03-21 11:28:27 -0400

    Bats can not longer go through walls

--- LegendOfZelda/Scripts/Enemy/Keese/BasicKeeseSprite.cs
+++ LegendOfZelda/Scripts/Enemy/Keese/BasicKeeseSprite.cs
@@ -24,14 +24,14 @@ namespace LegendOfZelda.Scripts.Enemy.Keese.Sprite
         {
             return direction switch
             {
-                0 => new Vector2(position.X, position.Y + moveSpeed * scale),
-                1 => new Vector2(position.X, position.Y - moveSpeed * scale),
-                2 => new Vector2(position.X - moveSpeed * scale, position.Y),
-                3 => new Vector2(position.X + moveSpeed * scale, position.Y),
-                4 => new Vector2(position.X + moveSpeed * scale, position.Y + moveSpeed * scale),
-                5 => new Vector2(position.X + moveSpeed * scale, position.Y - moveSpeed * scale),
-                6 => new Vector2(position.X - moveSpeed * scale, position.Y + moveSpeed * scale),
-                _ => new Vector2(position.X - moveSpeed * scale, position.Y - moveSpeed * scale),
+                0 => MovesPastWallsTest(position, new Vector2(position.X, position.Y + moveSpeed * scale), scale),
+                1 => MovesPastWallsTest(position, new Vector2(position.X, position.Y - moveSpeed * scale), scale),
+                2 => MovesPastWallsTest(position, new Vector2(position.X - moveSpeed * scale, position.Y), scale),
+                3 => MovesPastWallsTest(position, new Vector2(position.X + moveSpeed * scale, position.Y), scale),
+                4 => MovesPastWallsTest(position, new Vector2(position.X + moveSpeed * scale, position.Y + moveSpeed * scale), scale),
+                5 => MovesPastWallsTest(position, new Vector2(position.X + moveSpeed * scale, position.Y - moveSpeed * scale), scale),
+                6 => MovesPastWallsTest(position, new Vector2(position.X - moveSpeed * scale, position.Y + moveSpeed * scale), scale),
+                _ => MovesPastWallsTest(position, new Vector2(position.X - moveSpeed * scale, position.Y - moveSpeed * scale), scale),
             };
         }
         public override void Update(int scale)
