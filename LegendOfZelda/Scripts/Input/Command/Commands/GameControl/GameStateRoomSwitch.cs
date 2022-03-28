030ccd2f13df50a494c72819e1735dda23294ccf
benroth012 <benroth012@gmail.com>
2022-03-28 12:17:57 -0400

    Added Commands to change the gameState

---
+++ LegendOfZelda/Scripts/Input/Command/Commands/GameControl/GameStateRoomSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda.Scripts.Input.Command.Commands
+{
+    public class GameStateRoomSwitch : ICommand
+    {
+        private Game1 myGame;
+        public GameStateRoomSwitch(Game1 game)
+        {
+            myGame = game;
+        }
+        public void Execute()
+        {
+            myGame.Gstate = GameStateMachine.GameState.RoomSwitch;
+        }
+    }
+}
---
+++ LegendOfZelda/Scripts/Input/Command/Commands/GameControl/GameStateTriforce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda.Scripts.Input.Command.Commands
+{
+    public class GameStateTriforce : ICommand
+    {
+        private Game1 myGame;
+        public GameStateTriforce(Game1 game)
+        {
+            myGame = game;
+        }
+        public void Execute()
+        {
+            myGame.Gstate = GameStateMachine.GameState.Triforce;
+        }
+    }
+}
