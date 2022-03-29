05d414bd3b364778ab12c9bc28a43c2b502a8b15
drawingtoptarp <seanpburns@icloud.com>
2022-03-29 10:04:38 -0400

    Play sword beam sound

--- LegendOfZelda/Scripts/Sounds/SoundController.cs
+++ LegendOfZelda/Scripts/Sounds/SoundController.cs
@@ -7,7 +7,8 @@ namespace LegendOfZelda.Scripts.Sounds
     class SoundController
     {
         private Song music;
-        private SoundEffect openDoor, boomerang, getItem, getRupee, getTriforce, linkGetsHurt, useFire, placeAndExplodeBomb;
+        private SoundEffect openDoor, boomerang, getItem, getRupee, getTriforce, linkGetsHurt, useFire, placeAndExplodeBomb, 
+            swordBeam;
         private SoundEffectInstance boomerangInstance, useFireInstance;
         private static readonly SoundController instance = new SoundController(); 
         public static SoundController Instance => instance;
@@ -25,6 +26,7 @@ namespace LegendOfZelda.Scripts.Sounds
             linkGetsHurt = content.Load<SoundEffect>("Sounds/LinkSounds/playerGetsHurt");
             useFire = content.Load<SoundEffect>("Sounds/ItemSounds/useFire");
             placeAndExplodeBomb = content.Load<SoundEffect>("Sounds/ItemSounds/placeAndExplodeBomb");
+            swordBeam = content.Load<SoundEffect>("Sounds/ItemSounds/useSwordBeam");
         }
         public void StartDungeonMusic()
         {
@@ -37,6 +39,7 @@ namespace LegendOfZelda.Scripts.Sounds
             getTriforce.Play();
         }
         public void PlayLinkGetsHurtSound() { linkGetsHurt.Play(); }
+        public void PlaySwordBeamSound() { swordBeam.Play(); }
         public void PlayBombSound() { placeAndExplodeBomb.Play(); }
         public void PlayOpenDoorSound() { openDoor.Play(); }
         public void StartBoomerangSound()
