bd2b8338df88f1516988c0b6e3ec47a6614a909f
drawingtoptarp <seanpburns@icloud.com>
2022-04-08 09:46:34 -0400

    Some of room class methods offloaded to helper class

--- LegendOfZelda/Scripts/LevelManager/Room.cs
+++ LegendOfZelda/Scripts/LevelManager/Room.cs
@@ -1,11 +1,9 @@
 ﻿using LegendOfZelda.Scripts.Blocks;
-using LegendOfZelda.Scripts.Blocks.BlockSprites;
 using LegendOfZelda.Scripts.Enemy;
 using LegendOfZelda.Scripts.Items;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace LegendOfZelda.Scripts.LevelManager
 {
@@ -13,88 +11,29 @@ namespace LegendOfZelda.Scripts.LevelManager
     {
         private Vector2 keySpawnPos;
         private bool keySpawned = false;
+        private RoomObjectEditor roomObjectEditor;
         public IRoomBackground RoomBackground { get; private set; }
         public List<IItem> Items { get; private set; } = new List<IItem>();
         public List<IEnemy> Enemies { get; private set; } = new List<IEnemy>();
         public List<IBlock> Blocks { get; private set; } = new List<IBlock>();
 
-        public Room() { }
-        public void OpenSecretDoorUp()
-        {
-            foreach (IBlock block in Blocks)
-            {
-                if (block is SecretWallUpSprite) block.Disable();
-            }
-        }
-        public void OpenSecretDoorDown()
-        {
-            foreach (IBlock block in Blocks)
-            {
-                if (block is SecretWallDownSprite) block.Disable();
-            }
-        }
-        public void OpenSecretDoorLeft()
-        {
-            foreach (IBlock block in Blocks)
-            {
-                if (block is SecretWallLeftSprite) block.Disable();
-            }
-        }
-        public void OpenSecretDoorRight()
-        {
-            foreach (IBlock block in Blocks)
-            {
-                if (block is SecretWallRightSprite) block.Disable();
-            }
-        }
+        public Room() { roomObjectEditor = new RoomObjectEditor(Items, Enemies, Blocks); }
+        public void OpenSecretDoorUp() { roomObjectEditor.OpenSecretDoorUp(); }
+        public void OpenSecretDoorDown() { roomObjectEditor.OpenSecretDoorDown(); }
+        public void OpenSecretDoorLeft() { roomObjectEditor.OpenSecretDoorLeft(); }
+        public void OpenSecretDoorRight() { roomObjectEditor.OpenSecretDoorRight(); }
+        public void SpawnKey() { roomObjectEditor.SpawnKey(keySpawned, keySpawnPos); }
         public void AddObject(string type, string name, int xPos, int yPos, int adjacentRoom)
         {
-            if (type == "Item") AddItem(name, xPos, yPos);
-            else if (type == "Block") AddBlock(name, xPos, yPos, adjacentRoom);
-            else if (type == "Enemy") AddEnemy(name, xPos, yPos);
+            if (type == "Item") roomObjectEditor.AddItem(name, xPos, yPos);
+            else if (type == "Block") roomObjectEditor.AddBlock(name, xPos, yPos, adjacentRoom);
+            else if (type == "Enemy") roomObjectEditor.AddEnemy(name, xPos, yPos);
         }
         public void RemoveObject(string type, int index)
         {
-            if (type == "Item") RemoveItem(index);
-            else if (type == "Block") RemoveBlock(index);
-            else if (type == "Enemy") RemoveEnemy(index);
-        }
-        private void AddItem(string name, int xPos, int yPos)
-        {
-            Items.Add(ItemSpriteFactory.Instance.CreateItemFromString(name));
-            Items[^1].Position = new Vector2(xPos, yPos);
-        }
-        private void AddBlock(string name, int xPos, int yPos, int adjacentRoom)
-        {
-            Blocks.Add(BlockSpriteFactory.Instance.CreateBlockFromString(name));
-            Debug.WriteLine(Blocks[^1]);
-            Blocks[^1].Position = new Vector2(xPos, yPos);
-            if (name.Contains("Door") || name.Contains("Stairs")) {
-                Blocks[^1].AdjacentRoom = adjacentRoom;
-            }
-
-        }
-        private void AddEnemy(string name, int xPos, int yPos)
-        {
-            Enemies.Add(EnemySpriteFactory.Instance.CreateEnemyFromString(name));
-            Enemies[^1].position = new Vector2(xPos, yPos);
-        }
-        private void RemoveItem(int index)
-        {
-            Debug.WriteLine("Remove" + index);
-            Items.RemoveAt(index);
-        }
-        private void RemoveBlock(int index) { }
-        private void RemoveEnemy(int index) { Enemies.RemoveAt(index); }
-        public void SpawnKey() 
-        {
-            if (!keySpawned)
-            {
-                IItem key = ItemSpriteFactory.Instance.CreateKeySprite();
-                key.Position = keySpawnPos;
-                Items.Add(key);
-                keySpawned = true;
-            }
+            if (type == "Item") roomObjectEditor.RemoveItem(index);
+            else if (type == "Block") roomObjectEditor.RemoveBlock(index);
+            else if (type == "Enemy") roomObjectEditor.RemoveEnemy(index);
         }
         public void AddRoomBackground(int roomNumber, Vector2 screenOffset, int scale)
         {
---
+++ LegendOfZelda/Scripts/LevelManager/RoomObjectEditor.cs
@@ -0,0 +1,83 @@
+﻿using LegendOfZelda.Scripts.Blocks;
+using LegendOfZelda.Scripts.Blocks.BlockSprites;
+using LegendOfZelda.Scripts.Enemy;
+using LegendOfZelda.Scripts.Items;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.Scripts.LevelManager
+{
+    public class RoomObjectEditor
+    {
+        public List<IItem> Items { get; private set; } 
+        public List<IEnemy> Enemies { get; private set; }
+        public List<IBlock> Blocks { get; private set; }
+        public RoomObjectEditor(List<IItem> items, List<IEnemy> enemies, List<IBlock> blocks) 
+        {
+            Items = items;
+            Enemies = enemies;
+            Blocks = blocks;
+        }
+        public void OpenSecretDoorUp()
+        {
+            foreach (IBlock block in Blocks)
+            {
+                if (block is SecretWallUpSprite) block.Disable();
+            }
+        }
+        public void OpenSecretDoorDown()
+        {
+            foreach (IBlock block in Blocks)
+            {
+                if (block is SecretWallDownSprite) block.Disable();
+            }
+        }
+        public void OpenSecretDoorLeft()
+        {
+            foreach (IBlock block in Blocks)
+            {
+                if (block is SecretWallLeftSprite) block.Disable();
+            }
+        }
+        public void OpenSecretDoorRight()
+        {
+            foreach (IBlock block in Blocks)
+            {
+                if (block is SecretWallRightSprite) block.Disable();
+            }
+        }
+        public void AddItem(string name, int xPos, int yPos)
+        {
+            Items.Add(ItemSpriteFactory.Instance.CreateItemFromString(name));
+            Items[^1].Position = new Vector2(xPos, yPos);
+        }
+        public void AddBlock(string name, int xPos, int yPos, int adjacentRoom)
+        {
+            Blocks.Add(BlockSpriteFactory.Instance.CreateBlockFromString(name));
+            Blocks[^1].Position = new Vector2(xPos, yPos);
+            if (name.Contains("Door") || name.Contains("Stairs"))
+            {
+                Blocks[^1].AdjacentRoom = adjacentRoom;
+            }
+
+        }
+        public void AddEnemy(string name, int xPos, int yPos)
+        {
+            Enemies.Add(EnemySpriteFactory.Instance.CreateEnemyFromString(name));
+            Enemies[^1].position = new Vector2(xPos, yPos);
+        }
+        public void RemoveItem(int index) { Items.RemoveAt(index); }
+        public void RemoveBlock(int index) { }
+        public void RemoveEnemy(int index) { Enemies.RemoveAt(index); }
+        public void SpawnKey(bool keySpawned, Vector2 keySpawnPos)
+        {
+            if (!keySpawned)
+            {
+                IItem key = ItemSpriteFactory.Instance.CreateKeySprite();
+                key.Position = keySpawnPos;
+                Items.Add(key);
+                keySpawned = true;
+            }
+        }
+    }
+}
