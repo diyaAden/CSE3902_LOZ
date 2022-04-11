32e74518325cffb5e4b386eb33229d9d022c545f
Ben Roth <benroth012@gmail.com>
2022-04-11 19:53:14 -0400

    Messed up something with controllers?

--- LegendOfZelda/Scripts/Input/Controller/EndGameController.cs
+++ LegendOfZelda/Scripts/Input/Controller/EndGameController.cs
@@ -6,13 +6,13 @@ using Microsoft.Xna.Framework.Input;
 
 namespace LegendOfZelda.Scripts.Input.Controller
 {
-    public class KeyboardController : IController
+    public class EndGameController : IController
     {
         private Dictionary<Keys, ICommand> controllerMappings;
         private KeyboardState oldKeyState;
         private List<Keys> movementKeys = new List<Keys> { Keys.A, Keys.W, Keys.D, Keys.S};
         private List<Keys> arrowKeys = new List<Keys> { Keys.Up, Keys.Down, Keys.Left, Keys.Right };
-        public KeyboardController()
+        public EndGameController()
         {
             controllerMappings = new Dictionary<Keys, ICommand>();
         }
@@ -24,32 +24,16 @@ namespace LegendOfZelda.Scripts.Input.Controller
         {
             Keys[] keys = Keyboard.GetState().GetPressedKeys();
             KeyboardState keyState = Keyboard.GetState();
-            bool isMoving = false;
-
-            if (keys.Length == 0)
-            {
-                controllerMappings[Keys.F].Execute();
-            }
+            
             foreach (Keys key in keys)
             {
-                if (movementKeys.Contains(key) && !isMoving){
-                    controllerMappings[key].Execute();
-                    isMoving = true;
-                }
-                else if (arrowKeys.Contains(key) && !isMoving) {
-                    controllerMappings[key].Execute();
-                    isMoving = true;
-                }
-                else if (controllerMappings.ContainsKey(key) && keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key))
+                
+                if (controllerMappings.ContainsKey(key) && keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key))
                 {
                     controllerMappings[key].Execute();
                 } 
             }
             oldKeyState = keyState;
-            //isMoving = false;
-
-
-
         }
     }
 }
