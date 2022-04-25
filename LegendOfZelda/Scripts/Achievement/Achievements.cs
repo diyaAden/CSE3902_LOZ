fcf27a87c2a1ca607383e71faf41fedc3f7c49d8
pupu98 <77910363+pupu98@users.noreply.github.com>
2022-04-25 18:44:21 -0400

    .

--- LegendOfZelda/Scripts/Achievement/Achievements.cs
+++ LegendOfZelda/Scripts/Achievement/Achievements.cs
@@ -97,7 +97,7 @@ namespace LegendOfZelda.Scripts.Achievement
             }
             else
             {
-                AchivementText = "test";
+                AchivementText = "";
             }
         }
 
